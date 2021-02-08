db29b37ba380f31e57282e085b140a531415d500
arafatduha <arafatduha@gmail.com>
2021-02-08 23:03:29 +0600

    v2

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const displayMeals = meals => {
     const displayResult = document.getElementById('displayResult')
     meals.forEach(food => {
         const column = document.createElement('div')
-        column.className = "col-md-3"
+        column.className = "col-md-3 "
         const foodInfo = `
-        <div>
-            <h3> '${food.idMeal}' </h3>
-            <img src="${food.strMealThumb}">
+        <div onclick="displayDetails('${food.idMeal}')" class="items-image card w-100 overflow-hidden">
+        <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h4 class="card-title text-center"> Name: ${food.strMeal} </h4>
+            <h4 class="text-center">Food Category: ${food.strCategory}</h4>
+            <h5 text-center"> origin: ${food.strArea} </h5>
         </div>
         `
         column.innerHTML = foodInfo;
         displayResult.appendChild(column);
     });
 }
+
 
 //Event listener 
 var sf = document.getElementById('search-form');
@@ -75,4 +79,49 @@ const foodDetails = idMeal =>{
 //clear display
 const clearDisplay = () => {
     document.getElementById('displayResult').innerHTML="";
+}
+
+//Details display 
+const displayDetails = meals => {
+    const modalBody = document.getElementById('modalBody')
+    meals.forEach(food => {
+        const foodDetails = `
+        <div class="card w-100">
+            <div class="row g-0">
+                <div class="col-md-6">
+                    <img src="${food.strMealThumb}" class="card-img-top single-item-image" alt="...">
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h3 class="card-title">${food.strMeal} <span><button type="button" class="btn-close float-end" data-bs-dismiss="modal" aria-label="Close"></button></span></h3>
+                        <h5 class="card-title">Ingredients</h5>
+                        <ul class="list-unstyled">
+                            ${ingredientList(food)}
+                        </ul>
+                        <h3>!!Steps to Get the taste of it!!</h3>
+                        <p class="bg-primary">${food.strInstructions}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        modalBody.innerHTML = foodDetails;
+        const foodModal = new bootstrap.Modal(document.getElementById('foodModal'))
+        foodModal.show()
+    });
+}
+
+
+// Create Ingredient list
+const ingredientList = food => {
+    let li = ''
+
+    for (let i = 1; i <= 15; i++) {
+        let strIngredient = 'strIngredient' + i
+        if (food[strIngredient]) {
+            li = li + `<li> ${i}) ${food[strIngredient]}</li>`;
+        }
+    }
+    return li;
 }
